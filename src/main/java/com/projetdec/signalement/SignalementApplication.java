6f68b92cf71a12e84806c8eecb8d315704507514
dawoodNomena <dawoodnomena@gmail.com>
2021-12-27 18:32:33 +0300

    change

--- src/main/java/com/projetdec/signalement/SignalementApplication.java
+++ src/main/java/com/projetdec/signalement/SignalementApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SignalementApplication {
 
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(SignalementApplication.class, args);
 	}
