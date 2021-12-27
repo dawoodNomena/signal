e104ad5be689f4543014d65aa246ee623fb0fa0a
dawoodNomena <dawoodnomena@gmail.com>
2021-12-27 22:57:42 +0300

    d

--- src/main/java/com/projetdec/signalement/controllers/RegionController.java
+++ src/main/java/com/projetdec/signalement/controllers/RegionController.java
@@ -1,7 +1,7 @@
 package com.projetdec.signalement.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController
+@Controller
 @CrossOrigin
 public class RegionController{
     private final RegionRepository repository;
